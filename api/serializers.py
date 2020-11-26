9754b119a60607f7ff8955526de971b8c788859b
Julia <j.medvedeva18@gmail.com>
2020-11-26 21:24:10 +0100

    users

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from api.models import User
+
+class GetTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    confirmation_code = serializers.SlugField()
+
+
+class UsersSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name',
+            'username', 'bio', 'email', 'role')
