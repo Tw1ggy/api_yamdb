298108290ef95546dfc572e6991028c29aa1a5cc
Nik <plush337@gmail.com>
2020-12-03 11:44:48 +0000

    titles, genres, categories

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UsersViewSet, email_code, get_token
+from categories.views import CategoryViewSet
+from genres.views import GenreViewSet
+from users.views import UsersViewSet, email_code, get_token
+from titles.views import TitleViewSet
 
 router_v1 = DefaultRouter()
 router_v1.register('users', UsersViewSet, basename='users')
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('titles', TitleViewSet, basename='titles')
 
 v1_auth = [
     path('token/', get_token, name='token'),
